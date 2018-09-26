1bfed98516d467aa4f8066229bd1f6dbd620549f
ebinmoolan@gmail.com <bennye@tcd.ie>
2018-09-26 12:34:20 +0100

    Added testlca_btree which tests lca operations on a binary tree graph.

--- src/graph.rs
+++ src/graph.rs
@@ -12,3 +12,37 @@ pub fn lca<N, E>(
 ) -> Option<NodeIndex> {
     return None;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::lca;
+    use super::Graph;
+
+    #[test]
+    fn testlca_btree() {
+        let mut map = Graph::<&str, i32>::new();
+        let root = map.add_node("root");
+        let n1 = map.add_node("1");
+        let n2 = map.add_node("2");
+        let n3 = map.add_node("3");
+        let n4 = map.add_node("4");
+        let n5 = map.add_node("5");
+        let n6 = map.add_node("6");
+        map.extend_with_edges(&[
+            (root, n1),
+            (root, n2),
+            (n1, n3),
+            (n1, n4),
+            (n2, n5),
+            (n2, n6),
+        ]);
+        assert_eq!(true, lca(&map, root, n1, n5).is_some());
+        assert_eq!(root, lca(&map, root, n1, n5).unwrap());
+
+        assert_eq!(true, lca(&map, root, n6, n5).is_some());
+        assert_eq!(n2, lca(&map, root, n6, n5).unwrap());
+
+        assert_eq!(true, lca(&map, root, n3, n4).is_some());
+        assert_eq!(n1, lca(&map, root, n3, n4).unwrap());
+    }
+}
