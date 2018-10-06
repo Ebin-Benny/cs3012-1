8032485695fc2109479a5dc71e492d81464866b9
Ebin-Benny <ebinmoolan@gmail.com>
2018-10-06 15:13:38 +0100

    Updated tests to test lca on directed-acyclic graph

--- src/graph.rs
+++ src/graph.rs
@@ -24,7 +24,7 @@ fn neighbors<N, E>(graph: &Graph<N, E>, n: NodeIndex) -> LinkedList<(NodeIndex, 
 /// * `root`  - The root node of the binary tree.
 /// * `node1` - The first node to calculate lca.
 /// * `node2` - The second node to calculate lca.
-pub fn lca_btree<N, E>(
+pub fn lca<N, E>(
     graph: &Graph<N, E>,
     node1: NodeIndex,
     node2: NodeIndex,
@@ -52,102 +52,154 @@ pub fn lca_btree<N, E>(
             len = path2arr.len();
         }
 
-        let mut lca_btree = root;
+        let mut lca = root;
         for i in 0..len {
             if path1arr[i] == path2arr[i] {
-                lca_btree = path1arr[i]
+                lca = path1arr[i]
             } else {
                 break;
             }
         }
-        return Some(lca_btree);
+        return Some(lca);
     }
     return None;
 }
 
 #[cfg(test)]
 mod tests {
-    use super::lca_btree;
+    use super::lca;
     use super::Graph;
 
-    /// Tests normal operations of lca_btree on a connected graph structured as a binary tree.
+    /// Tests normal operations of lca on a connected graph structured as an directed acyclic graph.
     #[test]
-    fn testlca_btree() {
+    fn testlca_normal() {
         let mut map = Graph::<&str, i32>::new();
-        let root = map.add_node("root");
         let n1 = map.add_node("1");
         let n2 = map.add_node("2");
         let n3 = map.add_node("3");
         let n4 = map.add_node("4");
         let n5 = map.add_node("5");
         let n6 = map.add_node("6");
+        let n7 = map.add_node("7");
+        let n8 = map.add_node("8");
+
         map.extend_with_edges(&[
-            (root, n1),
-            (root, n2),
-            (n1, n3),
-            (n1, n4),
-            (n2, n5),
-            (n2, n6),
+            (n1,n2),
+            (n2,n2),
+            (n2,n3),
+            (n2,n4),
+            (n3,n5),
+            (n4,n6),
+            (n5,n7),
+            (n6,n7),
+            (n7,n8)
         ]);
-        assert_eq!(true, lca_btree(&map, n1, n5).is_some());
-        assert_eq!(root, lca_btree(&map, n1, n5).unwrap());
 
-        assert_eq!(true, lca_btree(&map, n6, n5).is_some());
-        assert_eq!(n2, lca_btree(&map, n6, n5).unwrap());
+        assert_eq!(true, lca(&map, n3, n2).is_some());
+        assert_eq!(n2, lca(&map, n3, n2).unwrap());
 
-        assert_eq!(true, lca_btree(&map, n3, n4).is_some());
-        assert_eq!(n1, lca_btree(&map, n3, n4).unwrap());
+        assert_eq!(true, lca(&map, n6, n3).is_some());
+        assert_eq!(n2, lca(&map, n6, n3).unwrap());
+
+        assert_eq!(true, lca(&map, n7, n5).is_some());
+        assert_eq!(n5, lca(&map, n7, n5).unwrap());
+
+        assert_eq!(true, lca(&map, n7, n4).is_some());
+        assert_eq!(n4, lca(&map, n7, n4).unwrap());
+
+        assert_eq!(true, lca(&map, n8, n4).is_some());
+        assert_eq!(n4, lca(&map, n8, n4).unwrap());
+
+        assert_eq!(true, lca(&map, n8, n2).is_some());
+        assert_eq!(n2, lca(&map, n8, n2).unwrap());
+
+        assert_eq!(true, lca(&map, n8, n5).is_some());
+        assert_eq!(n5, lca(&map, n8, n5).unwrap());
     }
 
     /// Tests that `None` is returned when nodes are not connected.
     #[test]
     fn testlca_notconn() {
         let mut map = Graph::<&str, i32>::new();
-        let root = map.add_node("root");
         let n1 = map.add_node("1");
-        let n3 = map.add_node("3");
+        let n2 = map.add_node("2");
         let n4 = map.add_node("4");
         let n5 = map.add_node("5");
         let n6 = map.add_node("6");
+        let n7 = map.add_node("7");
 
-        assert_eq!(false, lca_btree(&map, n1, n5).is_some());
+        assert_eq!(false, lca(&map, n2, n6).is_some());
 
-        assert_eq!(false, lca_btree(&map, n6, n5).is_some());
+        assert_eq!(false, lca(&map, n7, n6).is_some());
 
-        assert_eq!(false, lca_btree(&map, n3, n4).is_some());
+        assert_eq!(false, lca(&map, n4, n5).is_some());
     }
 
-    /// Tests that the same node is returned for when `node1` and `node2` are the same.
+    /// Tests that the correct node is returned when there are separate connected graphs, also tests if `None` is returned when Nodes are not connected.
     #[test]
-    fn testlca_samenode() {
+    fn testlca_separate() {
         let mut map = Graph::<&str, i32>::new();
-        let root = map.add_node("root");
-
-        assert_eq!(true, lca_btree(&map, root, root).is_some());
-        assert_eq!(root, lca_btree(&map, root, root).unwrap());
-    }
-
-    /// Tests that the function returns `None` when there is a cycle in the graph.
-    #[test]
-    fn testlca_structure() {
-        let mut map = Graph::<&str, i32>::new();
-        let root = map.add_node("root");
         let n1 = map.add_node("1");
         let n2 = map.add_node("2");
         let n3 = map.add_node("3");
         let n4 = map.add_node("4");
         let n5 = map.add_node("5");
         let n6 = map.add_node("6");
+        let n7 = map.add_node("7");
+        let n8 = map.add_node("8");
+
         map.extend_with_edges(&[
-            (root, n1),
-            (root, n2),
-            (n5, root),
-            (n4, root),
-            (n1, n3),
-            (n1, n4),
-            (n2, n5),
-            (n2, n6),
+            (n1,n2),
+            (n2,n3),
+            (n2,n4),
+            (n5,n6),
+            (n5,n7),
+            (n6,n8)
         ]);
-        assert_eq!(false, lca_btree(&map, n1, n5).is_some());
+
+        assert_eq!(true, lca(&map, n2, n4).is_some());
+        assert_eq!(n2, lca(&map, n2, n4).unwrap());
+
+        assert_eq!(true, lca(&map, n7, n8).is_some());
+        assert_eq!(n5, lca(&map, n7, n8).unwrap());
+
+        assert_eq!(false, lca(&map, n4, n6).is_some());
+    }
+
+    /// Tests that the same node is returned for when `node1` and `node2` are the same.
+    #[test]
+    fn testlca_samenode() {
+        let mut map = Graph::<&str, i32>::new();
+        let n1 = map.add_node("root");
+
+        assert_eq!(true, lca(&map, n1, n1).is_some());
+        assert_eq!(n1, lca(&map, n1, n1).unwrap());
+    }
+
+    /// Tests that the function returns `None` when there is a cycle in a path but functions normally otherwise.
+    #[test]
+    fn testlca_structure() {
+        let mut map = Graph::<&str, i32>::new();
+        let n1 = map.add_node("1");
+        let n2 = map.add_node("2");
+        let n3 = map.add_node("3");
+        let n4 = map.add_node("4");
+        let n5 = map.add_node("5");
+        let n6 = map.add_node("6");
+        let n7 = map.add_node("7");
+        map.extend_with_edges(&[
+            (n1, n2),
+            (n1, n3),
+            (n5, n1),
+            (n2, n4),
+            (n2, n5),
+            (n3, n6),
+            (n3, n7),
+        ]);
+        assert_eq!(false, lca(&map, n2, n6).is_some());
+
+        assert_eq!(true, lca(&map, n6, n7).is_some());
+        assert_eq!(n3, lca(&map, n6, n7).unwrap());
+
     }
 }
